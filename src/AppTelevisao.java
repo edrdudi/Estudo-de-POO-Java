c0db3530b429ee51241ebd39c4de9cf481aebdd5
Eduardo Morais <duudimorais@gmail.com>
2025-04-03 14:44:05 -0300

    Primeiras interações de POO

--- src/AppTelevisao.java
+++ src/AppTelevisao.java
@@ -1,4 +1,17 @@
-package PACKAGE_NAME;
-
 public class AppTelevisao {
+    public static void main (String[] args) {
+        int a;
+        String b;
+
+        Televisao t = new Televisao();
+
+        t.marca = "LG";
+        t.ligada = false;
+        t.volume = 0;
+        t.canal = 5;
+
+        t.ligar();
+        t.aumentarVolume();
+        t.avancarCanal();
+    }
 }
