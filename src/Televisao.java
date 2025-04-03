974368770bdc3f2ae2f5e52a42b11c778234aed1
Eduardo Morais <duudimorais@gmail.com>
2025-04-03 14:44:49 -0300

    Classe Televisão inicial

--- src/Televisao.java
+++ src/Televisao.java
@@ -1,4 +1,30 @@
-package PACKAGE_NAME;
-
 public class Televisao {
+
+    //atributos
+    String marca;
+    boolean ligada;
+    int volume;
+    int canal;
+
+    //metodos
+    void ligar(){
+        ligada = true;
+        System.out.println("A TV " + marca + " está ligada");
+    }
+
+    void aumentarVolume(){
+        if (ligada) {
+            volume++;
+            System.out.println("A TV" + marca + " está com volume " + volume);
+        } else {
+            System.out.println("Ligue a TV");
+        }
+    }
+
+    void avancarCanal(){
+        if (ligada){
+            canal++;
+            System.out.println("A TV " + marca + " está no canal " + canal);
+        }
+    }
 }
