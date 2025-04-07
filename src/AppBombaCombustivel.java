046c2784c7cbee833c3c268aa454514d745d0aed
Eduardo Morais <duudimorais@gmail.com>
2025-04-07 15:54:22 -0300

    Criação de construtor e melhorias no códigos - estudo e POO

--- src/AppBombaCombustivel.java
+++ src/AppBombaCombustivel.java
@@ -3,8 +3,14 @@ import java.util.Scanner;
 public class AppBombaCombustivel {
     public static void main (String[] args) {
         Scanner teclado = new Scanner(System.in);
-        BombaCombustível b1 = new BombaCombustível();
+        BombaCombustível b1 = new BombaCombustível("Gasolina", 5);
 
+        b1.puxarGancho();
+        b1.abastecerPorLitro(teclado.nextFloat());
+        b1.voltarGancho();
+
+        b1.puxarGancho();
         b1.abastecerPorValor(teclado.nextFloat());
+        b1.voltarGancho();
     }
 }
--- src/BombaCombustível.java
+++ src/BombaCombustível.java
@@ -1,53 +1,50 @@
 public class BombaCombustível {
     private String combustivel;
-    private float precoLitro = 5;
+    private float precoLitro;
     private float precoTotal;
     private float qtdLitros;
     private boolean emUso;
 
-    public void setCombustivel(String combustivel){
+    public BombaCombustível(String combustivel, float preco) {
         this.combustivel = combustivel;
+        this.precoLitro = preco;
+        this.qtdLitros = 0f;
+        this.precoTotal = 0f;
+        this.emUso = false;
     }
 
-    public String getCombustivel(){
-        return this.combustivel;
+    public void puxarGancho(){
+        this.emUso = true;
+        this.precoTotal = 0.0f;
+        this.qtdLitros = 0.0f;
+        System.out.println("Bomba " + this.combustivel + " pronta para uso");
+        System.out.println("Digite o valor a ser abastecido:");
     }
 
-    public void setPrecoLitro(float precoLitro){
-        this.precoLitro = precoLitro;
-    }
-
-    public float getPrecoLitro(){
-        return this.precoLitro;
-    }
-
-    public void setPrecoTotal(float precoTotal){
-        this.precoTotal = precoTotal;
-    }
-
-    public float getPrecoTotal(){
-        return this.precoTotal;
-    }
-
-    public void setQtdLitros(float qtdLitros){
-        this.qtdLitros = qtdLitros;
-    }
-
-    public float getQtdLitros(){
-        return this.qtdLitros;
-    }
-
-    public void setEmUso(boolean emUso){
-        this.emUso = emUso;
-    }
-
-    public boolean getEmUso(){
-        return this.emUso;
+    public void voltarGancho () {
+        this.emUso = false;
+        System.out.println("Combustivel: " + this.combustivel);
+        System.out.println("Preco do Litro: R$" + this.precoLitro);
+        System.out.println("Valor a pagar: R$" + this.precoTotal);
+        System.out.println("Litros abastecidos: " + qtdLitros);
+        System.out.println("");
     }
 
     public void abastecerPorValor(float valor){
-        qtdLitros = valor / precoLitro;
-        System.out.println("Valor a pagar: R$" +valor);
-        System.out.println("Litros abastecidos: " + qtdLitros);
+        if (this.emUso) {
+            this.precoTotal = valor;
+            this.qtdLitros = this.precoTotal / this.precoLitro;
+        } else {
+            System.out.println("Aguarde até a bomba ficar disponivel");
+        }
+    }
+
+    public void abastecerPorLitro(float litros) {
+        if(this.emUso) {
+            this.qtdLitros = litros;
+            precoTotal = precoLitro * litros;
+        } else {
+            System.out.println("Aguarde até a bomba ficar disponivel");
+        }
     }
 }
