f576e2a5728219409947e8ceef0d84d38d6c8927
Eduardo Morais <duudimorais@gmail.com>
2025-04-21 11:38:05 -0300

    Criação da classe Cliente e ClieteVIP para estudo de Herança

---
+++ src/Cliente.java
@@ -0,0 +1,36 @@
+public class Cliente {
+    private String nome;
+    private String email;
+    private float credito;
+
+    public Cliente(String nome, String email, float credito) {
+        this.nome = nome;
+        this.email = email;
+        this.credito = credito;
+    }
+
+    public void setCredito(float credito){
+        this.credito = credito;
+    }
+
+    public float getCredito(){
+        return this.credito;
+    }
+
+    public String toString(){
+        return "Cliente: " + this.nome + "(" + this.email + ") - R$" + this.credito;
+    }
+
+    public boolean fazerCompra(float valor){
+        if (valor > this.credito){
+            return false;
+        } else {
+            this.credito -= valor;
+            return true;
+        }
+    }
+
+    public void quitarDivida(float pagamento){
+        this.credito += pagamento;
+    }
+}
---
+++ src/ClienteVip.java
@@ -0,0 +1,29 @@
+public class ClienteVip extends Cliente{
+    private float creditoEspecial;
+
+    public ClienteVip(String nome, String email, float credito, float creditoEspecial){
+        super (nome, email, credito);
+        this.creditoEspecial = creditoEspecial;
+    }
+
+    public void setCreditoEspecial(){
+        this.creditoEspecial = creditoEspecial;
+    }
+
+    public float getCreditoEspecial(float creditoEspecial){
+        return this.creditoEspecial;
+    }
+
+    public String toString(){
+        return "(VIP)" + super.toString() + " // Credito Especial: R$" + this.creditoEspecial;
+    }
+
+    public boolean fazerCompra(float valor){
+        if (valor > super.getCredito() + this.creditoEspecial) {
+            return false;
+        } else {
+            super.setCredito(super.getCredito() - valor);
+            return true;
+        }
+    }
+}
