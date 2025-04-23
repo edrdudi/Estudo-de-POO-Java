d9709f8cc09aa6cbfba413d8865e18435f4b3550
Eduardo Morais <duudimorais@gmail.com>
2025-04-22 22:33:43 -0300

    Criacao de classe filha para estudo e Heranca

--- src/QuestaoMultiplaEscolha.java
+++ src/QuestaoMultiplaEscolha.java
@@ -1,4 +1,13 @@
-package PACKAGE_NAME;
+public class QuestaoMultiplaEscolha extends QuestaoSimples {
+    private String dica;
 
-public class QuestaoMultiplaEscolha {
+    public QuestaoMultiplaEscolha(String enunciado, String resposta, String dica) {
+        super (enunciado, resposta);
+        this.dica = dica;
+    }
+
+    @Override
+    public String aplicarQuestao() {
+        return super.aplicarQuestao() + " Dica: " + this.dica;
+    }
 }
