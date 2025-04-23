493844c4201973b6d026f34fa5276e20e54a1910
Eduardo Morais <duudimorais@gmail.com>
2025-04-22 22:34:13 -0300

    Criacao de outra classe filha

--- src/QuestaDica.java
+++ src/QuestaDica.java
@@ -1,4 +1,23 @@
-package PACKAGE_NAME;
+public class QuestaDica extends QuestaoSimples{
+    private String questao1;
+    private String questao2;
+    private String questao3;
+    private String questao4;
 
-public class QuestaDica {
+    public QuestaDica (String enunciado, String resposta, String questao1, String questao2, String questao3, String questao4){
+        super (enunciado, resposta);
+        this.questao1 = questao1;
+        this.questao2 = questao2;
+        this.questao3 = questao3;
+        this.questao4 = questao4;
+    }
+
+    @Override
+    public String aplicarQuestao() {
+        return super.aplicarQuestao() + " Qual a Alternativa correta?\n"
+                + this.questao1 + "\n"
+                + this.questao2 + "\n"
+                + this.questao3 + "\n"
+                + this.questao4;
+    }
 }
