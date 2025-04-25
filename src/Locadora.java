e5c660002d050d8e70f544f08b03ed428354135f
Eduardo Morais <duudimorais@gmail.com>
2025-04-24 22:43:48 -0300

    Criação de outra classe filha e de classe executável

--- src/Locadora.java
+++ src/Locadora.java
@@ -1,4 +1,20 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class Locadora {
+    public static void main(String[] args){
+        Scanner teclado = new Scanner(System.in);
+        Veiculo estoque[];
+
+        estoque = new Veiculo[5];
+
+        estoque[0] = new CarroNovo("Celta", "JDK0001", 50f);
+        estoque[1] = new Moto("CB300", "ISI1000",20f, 0.15f);
+        estoque[2] = new CarroNovo("Focus", "DUU02095", 55);
+        estoque[3] = new CarroNovo("Porsche", "POR2000", 500);
+        estoque[4] = new Moto("SR100RR", "SBR1000",150f, 0.10f);
+
+        for (Veiculo v : estoque) {
+            System.out.println("Veiculo: " + v.getModelo() + " / Aluguel: R$ " + v.calcularAluguel(teclado.nextInt()));
+        }
+    }
 }
--- src/Moto.java
+++ src/Moto.java
@@ -1,4 +1,12 @@
-package PACKAGE_NAME;
+public class Moto extends Veiculo{
+    private float fatorDesconto;
 
-public class Moto {
+    public Moto(String modelo, String placa, float valorDiaria, float fatorDesconto){
+        super(modelo, placa, valorDiaria);
+        this.fatorDesconto = fatorDesconto;
+    }
+
+    public float calcularAluguel(int qtdDias){
+        return (qtdDias * super.getValorDiaria()) - (qtdDias * super.getValorDiaria() * fatorDesconto);
+    }
 }
